44366ec79c8986af3039126d0a767c1467021a22
yevpy <eugene2piskun@gmail.com>
2025-05-21 16:38:03 +0300

    Add a new model UserFollowing

--- users/models.py
+++ users/models.py
@@ -7,3 +7,22 @@ class User(AbstractUser):
     profile_picture = models.ImageField(
         upload_to="profile_pictures", blank=True, null=True
     )
+
+
+class UserFollowing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    following_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="followers"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "following_user"], name="unique_following"
+            )
+        ]
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.user} is following {self.following_user}"
--- users/views.py
+++ users/views.py
@@ -1,7 +1,5 @@
 from rest_framework import generics, mixins, viewsets
-from rest_framework.decorators import permission_classes, api_view
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from users.models import User
